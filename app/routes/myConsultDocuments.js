ea05765221d01ba8505c862dd284aa9e814c0006
jcamilovd <jcvargasd@uqvirtual.edu.co>
2022-06-08 21:45:44 -0500

    intento de hacer un despliegue

--- app/routes/myConsultDocuments.js
+++ app/routes/myConsultDocuments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authToken = require('../middleware/auth-token');
-const consultDocumentsController = require('../controllers/myConsultDocuments-controller');
+const consultDocumentsController = require('../controllers/myconsultDocuments-controller.js');
 const router = express.Router();
 
 router.get('/',authToken.njwtAuth, consultDocumentsController.myConsultDocuments);
