c21bdac727d9882514595d134716d55f2a7caca5
jcamilovd <jcvargasd@uqvirtual.edu.co>
2022-06-09 21:22:39 -0500

    intento de hacer un deploy

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = 10101;
 
 const server = app.listen(process.env.PORT || port, () => {
   console.log(`Express server listening on port ${port}`);
-  
 });
 
-module.exports = app;W
+module.exports = app;
